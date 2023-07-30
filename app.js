c6d6eaeb064db38db5371d963ecfec80e7e415ce
dfunani <dfunani@live.co.za>
2023-07-30 21:01:39 +0200

    done

--- app.js
+++ app.js
@@ -2,60 +2,109 @@ const file = require("fs");
 require("dotenv").config();
 const axios = require("axios");
 
+//
 class MessageQueue {
   filename = "./queue.txt";
+  queue = {
+    unprocessed: [],
+    processed: [],
+  };
   constructor(strapiUpdateServer = process.env.STRAPI_UPDATER_SERVER) {
-    this.STRAPI_UPDATER_SERVER = strapiUpdateServer;
-    if (file.existsSync(this.filename)) {
-      this.queue = JSON.parse(file.readFileSync(this.filename));
-    } else {
-      this.queue = [];
-    }
+    this.strapiUpdateServer = strapiUpdateServer;
+    this.read();
   }
 
   add(queueItem) {
-    if (typeof queueItem === "object")
-      try {
-        this.queue = [...this.queue, ...queueItem.data];
-      } catch (error) {
-        console.log("Requested Queue Item could not be added", error);
-      }
+    if (typeof queueItem === "object") {
+      this.queue = {
+        processed: [...this.queue.processed],
+        unprocessed: [
+          ...queueItem.filter(
+            (item) =>
+              !this.queue.processed
+                .map((processed) => processed.id)
+                .includes(item.id)
+          ),
+        ],
+      };
+      console.log(
+        "Unprocessed MicroApps - " +
+          this.queue.unprocessed.length +
+          " Processed MicroApps - " +
+          this.queue.processed.length
+      );
+    }
   }
-  async process() {
-    this.read();
-    this.queue.forEach(async (item) => {
-      const poppedItem = this.queue.pop();
-      try {
-        const result = await axios.post(this.strapiUpdateServer, {
-          data: poppedItem,
+
+  process() {
+    const unprocessed = [...this.queue.unprocessed];
+    unprocessed.forEach((temp, index) => {
+      const poppedItem = this.queue.unprocessed.pop();
+      axios
+        .post(
+          this.strapiUpdateServer,
+          {
+            ...poppedItem
+          },
+          {
+            headers: { "Content-Type": "application/json", Accept: "*/*" },
+          }
+        )
+        .then((result) => {
+          if (result.status === 200) {
+            this.queue.processed.push(poppedItem);
+            console.log("Processed MicroApp: " + poppedItem.id)
+          } else {
+            this.queue.unprocessed.unshift(poppedItem);
+            console.log("Unprocessed MicroApp: " + poppedItem.id)
+          }
+          console.log(
+            "Unprocessed MicroApps - " +
+              this.queue.unprocessed.length +
+              " Processed MicroApps - " +
+              this.queue.processed.length
+          );
+          this.persist();
+        })
+        .catch((error) => {
+          this.queue.unprocessed.unshift(poppedItem);
+          console.log("Unprocessed MicroApp: " + poppedItem.id)
+          console.log(
+            "Unprocessed MicroApps - " +
+              this.queue.unprocessed.length +
+              " Processed MicroApps - " +
+              this.queue.processed.length
+          );
+
+          this.persist();
         });
-        if (result.status === 200) {
-          console.log(result.data);
-        } else {
-          this.queue.push(item);
-        }
-      } catch (error) {
-        this.queue.push(item);
-        console.log("Queue Item could not be process", error);
-      }
     });
   }
 
   persist() {
-    file.writeFile(this.filename, JSON.stringify(this.queue), () => {
-      console.log("Queue persisted");
-    });
+    file.writeFile(
+      this.filename,
+      JSON.stringify(this.queue, null, 2),
+      (err) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
   }
 
   read() {
     file.readFile(this.filename, (err, data) => {
       if (err) {
-        this.queue = [];
+        console.log("No File To Read");
       } else {
         try {
           this.queue = JSON.parse(data);
-        } catch (err) {
-          this.queue = [];
+        } catch (error) {
+          this.queue = {
+            unprocessed: [],
+            processed: [],
+          };
         }
       }
     });
@@ -73,34 +122,56 @@ class Strapi {
 
   async fetchOne(id) {
     try {
-      const result = await axios.get(this.strapiURL + this.strapiEndpoint + id);
+      const result = await axios.get(
+        this.strapiURL + this.strapiEndpoint + id + "/" + process.env.FILTERS
+      );
       return {
         status: "SUCCESS",
-        message: "Fetched One Item Without Issue",
-        result: result.data,
+        message: `Total MicroApps Fectched: ${result.data.data.length}`,
+        result: result.data.data.length,
       };
     } catch (error) {
       return {
         status: "ERROR",
-        message: error,
-        result: null,
+        message: `Total MicroApps Fectched: 0`,
+        result: error,
       };
     }
   }
 
   async fetchMany() {
+    let page = 1;
     try {
-      const result = await axios.get(this.strapiURL + this.strapiEndpoint);
+      let temp = await axios.get(
+        this.strapiURL +
+          this.strapiEndpoint +
+          process.env.FILTERS +
+          "&pagination[page]=" +
+          page.toString()
+      );
+
+      const result = temp;
+      while (page <= parseInt(result.data.meta.pagination.pageCount)) {
+        page++;
+        temp = await axios.get(
+          this.strapiURL +
+            this.strapiEndpoint +
+            process.env.FILTERS +
+            "&pagination[page]=" +
+            page.toString()
+        );
+        result.data.data = [...result.data.data, ...temp.data.data];
+      }
       return {
         status: "SUCCESS",
-        message: "Fetched One Item Without Issue",
-        result: result.data,
+        message: `Total MicroApps Fetched: ${result.data.data.length}`,
+        result: result.data.data,
       };
     } catch (error) {
       return {
         status: "ERROR",
-        message: error,
-        result: null,
+        message: `Total MicroApps Fetched: 0`,
+        result: error,
       };
     }
   }
@@ -111,7 +182,6 @@ const queue = new MessageQueue();
 strapi.fetchMany().then((app) => {
   if (app.status === "SUCCESS") {
     queue.add(app.result);
-    queue.persist();
     queue.process();
   }
 });
